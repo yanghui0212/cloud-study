f3238567ad951575bce00559df0f00d6e39a9502
yangqh <yangqianghui@eshippinggateway.com>
2021-05-28 17:06:12 +0800

    init

---
+++ common-service/basis-service/src/main/java/com/study/BasisServiceApplication.java
@@ -0,0 +1,16 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Admin
+ */
+@SpringBootApplication
+public class BasisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasisServiceApplication.class, args);
+    }
+
+}
